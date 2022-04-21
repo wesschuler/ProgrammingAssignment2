3211ff7cf00fd597d778448b450335ffefcb614c
Wesley Schuler <82042062+wesschuler@users.noreply.github.com>
2022-04-21 10:38:41 -0400

    Update cachematrix.R
    First attempt to load code for assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Computing the inverse of a square matrix can be done with the solve function
+## in R. For example, if X is a square invertible matrix, then solve(X) returns
+## its inverse.
+
+## Assumptions: the matrix supplied is always invertible.
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-add some code here
+      inverse <- NULL
+      push <- function(y) {
+            x <<- y
+            inverse <<- NULL
+      }
+      pull <- function() x
+      pushInverseMatrix <- function(InverseMatrix) inverse <<- InverseMatrix
+      pullInverseMatrix <- function() inverse
+      list (push = push,
+            pull = pull,
+            pushInverseMatrix = pushInverseMatrix,
+            pullInverseMatrix = pullInverseMatrix)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cacheSolve function should retrieve the
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverse <- x$pullInverseMatrix()
+      if (!is.null(inverse)) {
+            message("Cached data load: success!")
+            return(inverse)
+      }
+      myMatrix <- x$pull()
+      inverse <- solve(myMatrix, ...)
+      x$pushInverseMatrix(inverse)
+      inverse
+        
+## Return a matrix that is the inverse of 'x'
+        
 }
